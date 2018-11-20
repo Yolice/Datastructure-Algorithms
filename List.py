93a6c7aea6a83fa897009940105ea01887de7de4
Sennya <383340588@qq.com>
2018-11-20 19:50:08 +0900

    Update List.py

--- List.py
+++ List.py
@@ -3,6 +3,12 @@ class ListNode():
         self.data=data
         self.next=None
 
+class DoubleListNode():
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.rear=None
+
 
 class List():
     def __init__(self):
@@ -30,7 +36,7 @@ class List():
             self.last_node=new_node   ##用lastnode来记录当前最后一个节点，head始终是第一个节点
 
     def Delete(self,data):
-        self.heads=self.head      ##heads用来操作链表，head需要始终指向表头，若用来操作链表会造成head作为表头指向被删元素的前一个。
+        self.heads=self.head      ##heads类似于插入的lastnode用来操作链表，head需要始终指向表头，若用head来操作链表会造成head作为表头指向被删元素的前一个。
         if data != self.heads.data:    ##判断被删元素是否删表头元素
             while self.heads and self.heads.next.data != data:
                 self.heads=self.heads.next
@@ -51,5 +57,68 @@ class List():
         if node is not None:
             print(node.data)
             self.PrintList(node.next)
+
+
+class DoubleList(List):
+    def PreInsert(self,data):
+        if self.head is None:
+            self.head=ListNode(data)
+        else:
+            new_node=DoubleListNode(data)
+            new_node.next=self.head
+            self.head.rear=new_node
+            self.head = new_node
+
+    def Insert(self,data):   ##除了Insert和Delete需要重写，其他沿用父类即可
+        if self.head is None:
+           self.head=ListNode(data)
+           self.last_node=self.head
+        else:
+           new_node=DoubleListNode(data)
+           self.last_node.next=new_node   ##lastnode指向newnode
+           new_node.rear=self.last_node  ##newnode的前指针rear指向lastnode
+           self.last_node=new_node  ##newnode地址赋给lastnode，lastnode指向当前列表最后一个节
+
+
+    def Delete(self,data):
+        self.heads=self.head
+        if self.heads.data == data:
+            self.head=self.head.next
+            self.head.rear=None     ##若是头节点，则删除后rear指向None
+        elif self.last_node.data == data:
+            self.last_node.rear.next=None
+        else:
+            while self.heads and self.heads.next.data != data:
+                self.heads=self.heads.next
+            if self.heads:
+                self.heads.next=self.heads.next.next
+                self.heads.next.rear=self.heads
+            else:
+                return False
+
+
+
+
+
+
+
+
+a=DoubleList()
+for i in range(5):
+    a.PreInsert(i)
+
+
+
+a.PrintList(a.Get_Head())
+
+
+
+
+
+
+
+
+
+
 
 
