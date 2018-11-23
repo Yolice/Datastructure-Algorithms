b7a5c5c878276517aa3b59a9e159f303cabea443
Sennya <383340588@qq.com>
2018-11-23 19:23:46 +0900

    Update BinarySearchTree.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -5,7 +5,7 @@ class TreeNode():
         self.data=data
         self.left=None
         self.right=None
-
+        self.parent=None
 
 
 class BinarySearchTree():
@@ -41,12 +41,16 @@ class BinarySearchTree():
             else:
                 children=TreeNode(data)
                 root.left=children
+                children.parent=root
         elif data > root.data:
             if root.right:
                 self.add(root.right,data)
             else:
                 children=TreeNode(data)
                 root.right=children
+                children.parent=root
+        else:
+            return False
 
 
 
@@ -95,6 +99,11 @@ class BinarySearchTree():
         elif root.right is None:
             return root
 
+    def FreeNode(self,node):
+        node.left=None
+        node.right=None
+        node.parent=None
+
 
 
     def FindMinNode(self,root):
@@ -104,35 +113,41 @@ class BinarySearchTree():
             return root
 
 
-
     def DeleteTreeNone(self,data):
-        IndexNode=self.SearchNode(data)
-        if IndexNode is None:
-            print("No such tree node")
+        node=self.SearchNode(data)
+        if node:
+            if node.left and node.right and node.data == self.root.data:
+                replace_node=self.FindMinNode(node.right)
+                replace_node.left=node.left
+                replace_node.right=node.right
+                replace_node.parent.left=node.parent   ##如果写replace_node.parent=node.parent 即使replacenode的parent是None，但是parent的left依旧链接replacenode，
+                                                       ##因为二叉树的特性，replace节点一定是父节点的左子树，所以replacenode.parent.left=None即可以切断链接，最后节点由python回收机制回收
+                self.FreeNode(node)
+                self.root=replace_node   ##如果删除是根节点，要把代替的节点赋予给根节点
+
+
+
+
+
         else:
-            if IndexNode.left and IndexNode.right:
-                replace_node=self.FindMinNode(IndexNode.right)
-                replace_node.left=IndexNode.left
-                replace_node.right=IndexNode.right
-                IndexNode=None
-                replace_node=None
-            elif IndexNode.left is None and IndexNode.right:
-                IndexNode=IndexNode.right
-            elif IndexNode.left and IndexNode.right is None:
-                IndexNode=IndexNode.left
-            else:
-                IndexNode=None
-
-
+            return False   ##没有找到节点删除失败
 
 
 
 
 a=BinarySearchTree()
 
-for i in range(10):
-    a.CreateBinarySearchTree(i)
+a.CreateBinarySearchTree(30)
+a.CreateBinarySearchTree(20)
+a.CreateBinarySearchTree(25)
+a.CreateBinarySearchTree(24)
+a.CreateBinarySearchTree(23)
+a.CreateBinarySearchTree(50)
+a.CreateBinarySearchTree(35)
+a.CreateBinarySearchTree(50)
+a.CreateBinarySearchTree(60)
+a.CreateBinarySearchTree(70)
 
+a.DeleteTreeNone(30)
 
-a.DeleteTreeNone(4)
 a.PreTraversal(a.Get_root())
