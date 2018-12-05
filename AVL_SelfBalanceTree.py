c931ab46ff8ad91b7606b5cdda0c03ac1827923f
Sennya <383340588@qq.com>
2018-12-05 13:46:47 +0900

    Update AVL_SelfBalanceTree.py

--- AVL_SelfBalanceTree.py
+++ AVL_SelfBalanceTree.py
@@ -120,9 +120,9 @@ class Self_Balance_BST:
                 node.parent.right=inbalance_left_node
             inbalance_left_node.parent=node.parent
             node.parent = inbalance_left_node
-        inbalance_left_node.right=node   ##上下顺序不能颠倒
+        inbalance_left_node.right=node
         node.left = temple_node
-        if temple_node:   ##需要重新定位temple的parent
+        if temple_node:
             temple_node.parent=node
 
 
@@ -190,7 +190,7 @@ class Self_Balance_BST:
 
 
 
-    def Get_node_balancefator(self,root,count):   ##当前递归会保存当前层次的高度
+    def Get_node_balancefator(self,root,count):
         if root:     ##一切建立在这个节点是存在的情况下
             leftheight=self.PrintHeight(root.left,count)
             self.height=0
@@ -221,6 +221,8 @@ class Self_Balance_BST:
             return self.SearchStep(self.root,data)      ##从这行开始，到下面searchstep函数都需要return接力返回结果，递归后的函数如果没有return默认返回None
 
 
+
+
     def SearchStep(self,root,data):
         if root.data == data:
             return root
@@ -228,6 +230,82 @@ class Self_Balance_BST:
             return self.SearchStep(root.left,data)         ##必须要return
         elif data > root.data and root.right is not None:
             return self.SearchStep(root.right,data)    ##必须要return
+
+
+    def FreeNode(self,node):
+        node.left=None
+        node.right=None
+        node.parent=None
+
+
+
+    def FindMinNode(self,root):
+        if root.left:
+            return self.FindMinNode(root.left)
+        elif root.left is None:
+            return root
+
+
+
+    def FindMaxNode(self,root):
+        if root.right:
+            return self.FindMaxNode(root.right)   ##树是一种递归性质的结构，所以在获取树中的一个节点时候要接力return的值
+        elif root.right is None:
+            return root
+
+
+
+
+    def DeleteTreeNone(self,data):   ##作为一种简单的方法，可以交换被删除节点的值和代替节点的值，然后删除代替节点
+        node=self.SearchNode(data)
+        if node:
+            if node.left and node.right:   ##假如要删除的节点左右子树都存在的情况
+                replace_node=self.FindMinNode(node.right)
+                replace_node.left=node.left
+                replace_node.right=node.right
+                if node.data == self.root.data:            ##要删除的是根节点的情况
+                    replace_node.parent.left=None          ##如果写replace_node.parent=None,那么parent的left依旧链接replacenode，所以要写出parent.left链向目标
+                                                           ##因为二叉树的特性，replace节点一定是父节点的左子树，所以replacenode.parent.left=None即可以切断链接，最后节点由python回收机制回收
+                    self.FreeNode(node)
+                    self.root=replace_node                 ##如果删除是根节点，那么代替节点就是新的根节点00
+
+                else:                                      ##在删除之前要找到被删除点是左子树或者是右子树
+                    if node.parent.left and node.parent.left.data == node.data:
+                        replace_node.parent.left=None
+                        node.parent.left=replace_node
+                        self.FreeNode(node)
+                    elif node.parent.right and node.parent.right.data == node.data:
+                        replace_node.parent.left=None
+                        node.parent.right=replace_node
+                        self.FreeNode(node)
+            elif node.left and node.right is None:   ##只有单一左子树存在时候
+                if node.parent.left and node.parent.left.data == node.data:   ##判断左子树或者右紫薯
+                    node.parent.left=node.left
+                    self.FreeNode(node)
+                elif node.parent.right and node.parent.right.data == node.data:
+                    node.parent.right=node.left
+                    self.FreeNode(node)
+            elif node.right and node.left is None:
+                if node.parent.left and node.parent.left.data == node.data:
+                    node.parent.left=node.right
+                    self.FreeNode(node)
+                elif node.parent.right and node.parent.right.data == node.data:
+                    node.parent.right=node.right
+                    self.FreeNode(node)
+            elif node.left is None and node.right is None:  ##如果没有左右子树
+                if node.parent.left and node.parent.left.data == node.data:
+                    node.parent.left=None
+                    self.FreeNode(node)
+                elif node.parent.right and node.parent.right.data == node.data:
+                    node.parent.right=None
+                    self.FreeNode(node)
+            self.Rotation()
+
+        else:
+            return False  ##没有找到节点删除失败
+
+
+
 
 
     def reset_root(self,node):
@@ -257,5 +335,3 @@ a.Create_SelfBalanceTree(10)
 a.Create_SelfBalanceTree(8)
 a.Create_SelfBalanceTree(9)
 
-
-a.MidTraversal(a.Get_root())
