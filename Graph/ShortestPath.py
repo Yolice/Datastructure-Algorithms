6a760c457aa5170893fc18b61f2014d4b854463c
Sennya <383340588@qq.com>
2018-12-28 20:24:16 +0900

    Update ShortestPath.py

--- Graph/ShortestPath.py
+++ Graph/ShortestPath.py
@@ -38,19 +38,19 @@ class ShortestPath:
 
 
     def Dijkstra(self,AdjacencyMatrix,Vertexs,index): ##index代表想要找哪个顶点最短路径的在矩阵中索引值
-        distance=AdjacencyMatrix[index]
+        distance=AdjacencyMatrix[index] ##哪个当作源点，就取哪一行
         self.confirm_vertex.append(distance[index]) ##默认初始顶点
-        for i in range(len(distance)-2):
+        for i in range(len(distance)-2): ##n个点，只需要操作n-2次就行了
             temp=[]
-            minimum_cost_weight=self.Find_Min(distance,self.confirm_vertex) ##每个顶点对应最小的权值
-            minimum_cost_vertex=distance.index(minimum_cost_weight) ##最小权值对应的顶点
+            minimum_cost_weight=self.Find_Min(distance,self.confirm_vertex) ##找出当前代价最小的一条边
+            minimum_cost_vertex=distance.index(minimum_cost_weight) ##代价最小一条边对应的顶点
             for k in AdjacencyMatrix[minimum_cost_vertex]:
                 if k!=INF and k!= 0:
-                    temp.append(AdjacencyMatrix[minimum_cost_vertex].index(k)) ##找到最短点的边关系的权值
-            for j in temp:
+                    temp.append(AdjacencyMatrix[minimum_cost_vertex].index(k)) ##代价最小边对应顶点的边的关系
+            for j in temp: ##依次比较，如果在通过代价最小边对应顶点比直接去耗费代价小，那么更新对应的值(具体解释看onenote笔记最短源路径)
                 if AdjacencyMatrix[0][j] > AdjacencyMatrix[0][minimum_cost_vertex]+AdjacencyMatrix[minimum_cost_vertex][j]:
                     distance[j]=AdjacencyMatrix[0][minimum_cost_vertex]+AdjacencyMatrix[minimum_cost_vertex][j]
-            self.confirm_vertex.append(distance[minimum_cost_vertex])
+            self.confirm_vertex.append(distance[minimum_cost_vertex]) ##把已经找到点对应的权值加入数组，以后选最小权值跳过这个数组的值
 
 
         return distance
@@ -76,12 +76,6 @@ class ShortestPath:
 
 
 
-
-
-
-
-
-
-print(ShortestPath().Dijkstra(Matrix,Vertexs,0)) ##找出顶点0的最短路径信息
+#print(ShortestPath().Dijkstra(Matrix,Vertexs,0)) ##找出顶点0的最短路径信息
 #pprint.pprint(Matrix)
 
